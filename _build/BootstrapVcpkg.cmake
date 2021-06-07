263c1922697021c05d717f8ef1c33704887da97f
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-06-07 15:57:23 +0200

    Ne fait pas le bootstrap si l'exécutable existe.

--- _build/BootstrapVcpkg.cmake
+++ _build/BootstrapVcpkg.cmake
@@ -5,8 +5,20 @@ if (NOT VCPKG_BUILD_DIR)
 endif()
 message(STATUS "VCPKG_BUILD_DIR = ${VCPKG_BUILD_DIR}")
 message(STATUS "Env: VCPKG_BUILD_DIR = $ENV{VCPKG_BUILD_DIR}")
+
 set(BOOTSTRAP_COMMAND "/bin/sh" "bootstrap-vcpkg.sh")
+set(VCPKG_EXE "vcpkg")
 if (WIN32)
   set(BOOTSTRAP_COMMAND "bootstrap-vcpkg.bat")
+  set(VCPKG_EXE "vcpkg.exe")
 endif()
+
+# Si l'exécutable 'vcpkg' existe, pas la peine de faire le bootstrap.
+set(VCPKG_FULL_PATH "${VCPKG_BUILD_DIR}/${VCPKG_EXE}")
+message(STATUS "Check if vcpkg need to be boostraped. Expected file is '${VCPKG_FULL_PATH}'")
+if(EXISTS "${VCPKG_FULL_PATH}")
+  message(STATUS "File '${VCPKG_FULL_PATH}' exists. Do not bootstrap")
+  return()
+endif()
+
 do_command(${BOOTSTRAP_COMMAND} WORKING_DIRECTORY "${VCPKG_BUILD_DIR}")
