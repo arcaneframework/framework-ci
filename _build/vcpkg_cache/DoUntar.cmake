9f493ee2462ccd7ac4cca08ea072452d47b25aa8
Gilles Grospellier <Gilles.Grospellier@cea.fr>
2021-06-07 12:32:13 +0200

    Affiche la liste des fichiers du package nuget.

--- _build/vcpkg_cache/DoUntar.cmake
+++ _build/vcpkg_cache/DoUntar.cmake
@@ -47,6 +47,8 @@ endif()
 
 do_command(${MONO_EXEC} "${NUGET_COMMAND}" install -Source GitHub -Verbosity detailed -NonInteractive -Version "${HASH_PACKAGE_NAME}" -OutputDirectory "${VCPKG_BUILD_DIR}" VcpkgCache)
 
-do_command(tar xf "vcpkg_cache.tar" --exclude "downloads" --exclude "packages" .
-  WORKING_DIRECTORY "${VCPKG_BUILD_DIR}")
+message(STATUS "Get list fo files")
+file(GLOB_RECURSE FILE_LIST LIST_DIRECTORIES true "${VCPKG_BUILD_DIR}/*")
+message(STATUS "List: ${FILE_LIST}")
 
+do_command(tar xf "vcpkg_cache.tar" WORKING_DIRECTORY "${VCPKG_BUILD_DIR}")
