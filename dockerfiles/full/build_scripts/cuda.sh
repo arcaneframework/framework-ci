ba735f79b979fbdfe9b7bdc09056ec778f9c472d
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-05-21 16:15:48 +0200

    [Full] Fix mistake for CUDA ARM64

--- dockerfiles/full/build_scripts/cuda.sh
+++ dockerfiles/full/build_scripts/cuda.sh
@@ -7,6 +7,16 @@ set -e
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
+ARCH=`uname -m`
+
+if [ "$ARCH" = "x86_64" ]; then
+  ARCH_A="x86_64"
+elif [ "$ARCH" = "aarch64" ]; then
+  ARCH_A="sbsa"
+else
+  exit 1
+fi
+
 # À noter que CUDA 13.1 est compatible avec g++-15 max et clang-20 max.
 # Le driver n'est pas inclus. Le driver de l'host sera utilisé.
 # Le package libcurand-dev-13-1 est nécessaire pour AdaptiveCpp.
@@ -28,10 +38,10 @@ apt-get -y install \
 #---------------------------------------------------------------------------
 
 # Patch pour CUDA 13.1 et GCC 15 (https://github.com/ggml-org/llama.cpp/issues/19100)
-sed -i 's/__func__(double rsqrt(double a));/__func__(double rsqrt(double a) noexcept(true));/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
-sed -i 's/__func__(float rsqrtf(float a));/__func__(float rsqrtf(float a) noexcept(true));/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
-sed -i '629s/rsqrt(double x);/rsqrt(double x) noexcept(true);/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
-sed -i '653s/);/) noexcept(true);/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
+sed -i 's/__func__(double rsqrt(double a));/__func__(double rsqrt(double a) noexcept(true));/' /usr/local/cuda-13.1/targets/${ARCH_A}-linux/include/crt/math_functions.h
+sed -i 's/__func__(float rsqrtf(float a));/__func__(float rsqrtf(float a) noexcept(true));/' /usr/local/cuda-13.1/targets/${ARCH_A}-linux/include/crt/math_functions.h
+sed -i '629s/rsqrt(double x);/rsqrt(double x) noexcept(true);/' /usr/local/cuda-13.1/targets/${ARCH_A}-linux/include/crt/math_functions.h
+sed -i '653s/);/) noexcept(true);/' /usr/local/cuda-13.1/targets/${ARCH_A}-linux/include/crt/math_functions.h
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
