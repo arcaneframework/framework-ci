e53e651dc12b162484a7b80710d6ac1b2f5232fd
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-05-21 11:19:23 +0200

    [Full] Add patch for CUDA

--- dockerfiles/full/build_scripts/cuda.sh
+++ dockerfiles/full/build_scripts/cuda.sh
@@ -27,6 +27,15 @@ apt-get -y install \
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
 
+# Patch pour CUDA 13.1 et GCC 15 (https://github.com/ggml-org/llama.cpp/issues/19100)
+sed -i 's/__func__(double rsqrt(double a));/__func__(double rsqrt(double a) noexcept(true));/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
+sed -i 's/__func__(float rsqrtf(float a));/__func__(float rsqrtf(float a) noexcept(true));/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
+sed -i '629s/rsqrt(double x);/rsqrt(double x) noexcept(true);/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
+sed -i '653s/);/) noexcept(true);/' /usr/local/cuda-13.1/targets/x86_64-linux/include/crt/math_functions.h
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
 # Cleanup
 cd /
 rm -rf /var/lib/apt/lists/*
