78d3b435d3213aebc893c23fa94c5876f31c981a
Alexandre l'Heritier <alexandre.lheritier@cea.fr>
2026-06-17 16:09:41 +0200

    [Full] Remove symlink if useless

--- dockerfiles/full/scripts/configure_cuda_driver.sh
+++ dockerfiles/full/scripts/configure_cuda_driver.sh
@@ -14,8 +14,24 @@ ARCH=`uname -m`
 
 if [ -e /usr/lib/libcuda.so.1 ]; then
   echo "Nvidia driver found (1)."
+  if [ -L "/usr/local/cuda/lib64/libcuda.so.1" ]; then
+    LINK=$(readlink "/usr/local/cuda/lib64/libcuda.so.1")
+    if [ "$LINK" = "/usr/local/cuda/lib64/stubs/libcuda.so" ]; then
+      echo "Removing symlink..."
+      rm -rf /usr/local/cuda/lib64/libcuda.so.1
+    fi
+  fi
+
 elif [ -e /usr/lib/${ARCH}-linux-gnu/libcuda.so.1 ]; then
   echo "Nvidia driver found (2)."
+  if [ -L "/usr/local/cuda/lib64/libcuda.so.1" ]; then
+    LINK=$(readlink "/usr/local/cuda/lib64/libcuda.so.1")
+    if [ "$LINK" = "/usr/local/cuda/lib64/stubs/libcuda.so" ]; then
+      echo "Removing symlink..."
+      rm -rf /usr/local/cuda/lib64/libcuda.so.1
+    fi
+  fi
+
 else
   echo "Nvidia driver not found. Use stub libcuda.so lib."
   if [ -e /usr/local/cuda/lib64/libcuda.so.1 ]; then
